2c3048dbfca04ffa637a4cd9c0aba79fd283da17
TaIFeel <94287800+TaIFeel@users.noreply.github.com>
2022-09-05 08:52:14 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -1,4 +1,4 @@
-from vkaudiotoken import get_kate_token, get_vk_official_token
+from vkaudiotoken import get_vk_official_token
 import requests
 
 login = '' # your vk login, e-mail or phone number
@@ -8,13 +8,13 @@ sess = requests.session()
 
 
 try:
-    info = get_kate_token(login, password)
+    info = get_vk_official_token(login, password)
 
 except Exception as exc:
     if exc.extra["error"] == "need_validation":
         code = input("SMS CODE: >>> ")
 
-    info = get_kate_token(login, password, code)
+    info = get_vk_official_token(login, password, code)
 
 sess.headers.update({'User-Agent': info["user_agent"]})
 
